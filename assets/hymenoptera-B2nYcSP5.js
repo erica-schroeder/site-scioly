c49388c65e184e33eb7034c0bbfa1e929b3b9264
Erica Schroeder <schroeder.erica.m@gmail.com>
2025-10-28 08:03:44 -0400

    Updates

---
+++ assets/hymenoptera-B2nYcSP5.js
@@ -0,0 +1,8 @@
+const e=[{question:`Which order contains the following insects?
+
+Bees
+Ants
+Wasps
+Ichneumon
+Sawflies
+Horntails`,answer:"Hymenoptera"},{question:"What does hymenoptera mean?",answer:"Membrane-wing"},{question:"What type of metamorphosis does hymenoptera undergo?",answer:"Complete"},{question:"Which order is this insect in?",answer:"Hymenoptera (bee)",image:"bee.jpg"},{question:"Which order is this insect in?",answer:"Hymenoptera (wasp)",image:"wasp.jpg"},{question:"Which order is this insect in?",answer:"Hymenoptera (ant)",image:"ant.jpg"},{question:"What type of mouthparts does hymenoptera have?",answer:'Chewing (and bees have "tongues" too)',image:"ant-mouthparts.jpg"}];export{e as default};
---
+++ assets/trichoptera-MireYvAT.js
@@ -0,0 +1,1 @@
+const e=[{question:"Where do caddisfly larvae live?",answer:"Lakes and rivers"},{question:"What does tricoptera mean?",answer:"Hair wing"},{question:"What type of metamorphosis does trichoptera undergo?",answer:"Complete"},{question:"What is unique about caddisfly larvae?",answer:"They build cases to protect themselves"}];export{e as default};
